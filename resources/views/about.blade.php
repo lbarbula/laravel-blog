64a116e63466f82b7712463bf240ea48faa14ff1
Lucas Barbula <lucasbarbula@Lucass-MacBook-Pro.local>
2018-03-25 13:15:20 -0600

    Setting up initial routes.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+
+        <title>Hey YOOOOO</title>
+
+    </head>
+    <body>
+            <h1>about<h1>
+    </body>
+</html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,7 +8,11 @@
     <body>
         <ul>
             @foreach($tasks as $task)
-                <li>{{ $task->body }}</li>
+                <li>
+                    <a href="tasks/{{ $task->id }}">
+                        {{ $task->body }}
+                    </a>
+                </li>
             @endforeach
         </ul>
     </body>
